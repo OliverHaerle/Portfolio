ca54c90994a0e2a4ab47d75bfb5d9ef0da472ef9
OliverHaerle <136490738+OliverHaerle@users.noreply.github.com>
2023-12-11 12:19:55 +0100

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent]
 })
 export class AppComponent {
   title = 'Portfolio';
--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
     selector: 'app-LandingPage',
     standalone: true,
     imports: [CommonModule],
-    template: '<section><h1>Portfolio</h1><h2>Oliver Härle</h2></section>',
+    templateUrl: './landingPage.component.html',
     styleUrls: ['./landingPage.component.scss']
 }) //decorator / typescript-specific
 
