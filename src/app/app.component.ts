f06a449a5d56b77b9d2914355cc961b9f4cd61ac
OliverHaerle <136490738+OliverHaerle@users.noreply.github.com>
2023-12-13 15:37:03 +0100

    created pokedex component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,15 @@ import { LandingPageComponent } from './landingPage/landingPage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillSetComponent } from './skill-set/skill-set.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent, SkillSetComponent]
+    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent, PokedexComponent]
 })
 export class AppComponent {
   title = 'Portfolio';
