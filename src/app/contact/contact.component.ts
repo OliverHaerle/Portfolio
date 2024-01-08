dc19837163462397272d914e0bf0a2440cda3806
OliverHaerle <136490738+OliverHaerle@users.noreply.github.com>
2024-01-08 17:15:42 +0100

    added sent animation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,51 +6,65 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
   @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('button') button!: ElementRef;
+  @ViewChild('contactMessage') contactMessage!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void { }
 
+async sendMail() {
+  console.log('sending mail', this.myForm);
 
-  async sendMail() {
-    console.log('sending mail', this.myForm);
+  let nameField = this.nameField.nativeElement;
+  let emailField = this.emailField.nativeElement;
+  let messageField = this.messageField.nativeElement;
+  let button = this.button.nativeElement;
 
-    let nameField = this.nameField.nativeElement;
-    let emailField = this.emailField.nativeElement;
-    let messageField = this.messageField.nativeElement;
-    let button = this.button.nativeElement;
+  this.disableButtons(nameField, emailField, messageField, button)
 
-    this.disableButtons(nameField, emailField, messageField, button)
-
-    let fd = new FormData();
-    fd.append('name', nameField.value)
-    fd.append('email', emailField.value)
-    fd.append('message', messageField.value)
+  let fd = new FormData();
+  fd.append('name', nameField.value)
+  fd.append('email', emailField.value)
+  fd.append('message', messageField.value)
 
 
-    await fetch('https://oliverhaerle.at/send_mail/send_mail/send_mail.php', {
-      method: 'POST',
-      body: fd
-    })
+  await fetch('https://oliverhaerle.at/send_mail/send_mail/send_mail.php', {
+    method: 'POST',
+    body: fd
+  })
 
-    setTimeout(() => {
-      nameField.disabled = false;
-      emailField.disabled = false;
-      messageField.disabled = false;
-      button.disabled = false;
-    }, 3000);
-  }
-
-  disableButtons(nameField: { disabled: boolean; }, emailField: { disabled: boolean; }, messageField: { disabled: boolean; }, button: { disabled: boolean; }) {
-    nameField.disabled = true;
-    emailField.disabled = true;
-    messageField.disabled = true;
-    button.disabled = true;
-  }
-
+  setTimeout(() => {
+    nameField.disabled = false;
+    emailField.disabled = false;
+    messageField.disabled = false;
+    button.disabled = false;
+  }, 3000);
 }
+
+disableButtons(nameField: { disabled: boolean; }, emailField: { disabled: boolean; }, messageField: { disabled: boolean; }, button: { disabled: boolean; }) {
+  nameField.disabled = true;
+  emailField.disabled = true;
+  messageField.disabled = true;
+  button.disabled = true;
+}
+
+animation() {
+  let message = this.contactMessage.nativeElement;
+  this.renderer.addClass(message, 'flyIn')
+
+  setTimeout(() => {
+    this.renderer.addClass(message, 'flyOut')
+  }, 2500);
+  setTimeout(() => {
+    this.renderer.removeClass(message, 'flyIn');
+    this.renderer.removeClass(message, 'flyOut');
+  }, 3000);
+}
+}
+
